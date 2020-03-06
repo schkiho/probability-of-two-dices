6aef5a8d01498842f2376706f454646b92be4685
schkiho <schkiho@yahoo.de>
2020-03-06 12:54:47 +0100

    finish

--- problemA.js
+++ problemA.js
@@ -1,14 +1,3 @@
-// const readline = require('readline');
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// rl.on('line', line => {
-//   let nums = line.split(' ');
-//   let N = parseInt(nums[0]);
-//   let M = parseInt(nums[1]);
 getProbability = (N, M) => {
   let maxSum = N + M;
   let minSum = 2;
